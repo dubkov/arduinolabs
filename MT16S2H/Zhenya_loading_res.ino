c86ddd8ba03d274129772a4217c0c0018587edc1
Илья Дубков <dubkov@users.noreply.github.com>
2014-10-25 14:12:41 +0700

    Update Zhenya_loading_res.ino

--- MT16S2H/Zhenya_loading_res.ino
+++ MT16S2H/Zhenya_loading_res.ino
@@ -1,33 +1,36 @@
 #include <LiquidCrystal.h>
 
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(4, 5, 10, 11, 12, 13);
 
 void setup() {
-  // set up the LCD's number of columns and rows: 
+  randomSeed(analogRead(A1)); 
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd.print("Loading... ");
-  for (int g=0;g<101;g++){
-  lcd.print(g);
-  lcd.print("%");
-    delay (250);  
-    lcd.setCursor (12,0);
-  }
-//  delay(25000);
+  int percentOfBreak = 71;
   
+  for (int percent=0;percent<=100;percent++){
+    lcd.print(percent);
+    lcd.print("%");
+    if (percent <= percentOfBreak){
+      delay (random(10,800));  
+    }
+    else {
+      delay (100);
+    }
+    lcd.setCursor (12,0);
+    
+    if (percent == percentOfBreak){
+      delay (4000);
+    }
+  }
 }
 
 void loop() {
   lcd.clear();
   lcd.setCursor (0,0);
   lcd.print ("A0 value = ");
-int T=analogRead (A0); 
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+  int T=analogRead (A0); 
   lcd.setCursor(12, 0);
   lcd.print (T);
   delay (100);
-  
-  // print the number of seconds since reset:
 }
