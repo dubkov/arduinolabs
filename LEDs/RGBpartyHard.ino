af5db4309ac5ea4906e9e9ff75074cec384f8d05
Илья Дубков <dubkov@users.noreply.github.com>
2014-10-27 11:41:50 +0700

    все закоментировал

--- LEDs/RGBpartyHard.ino
+++ LEDs/RGBpartyHard.ino
@@ -1,15 +1,19 @@
+#define R_PIN 10                    //пин с красным светодиодом 10
+#define G_PIN 9                     //пин с зеленым светодиодом 9
+#define B_PIN 3                     //пин с синим светодиодом 3
+
 void setup(){
-  randomSeed (0);
+  randomSeed (0);                   //инициализация рандома
 }
 
 void loop(){
-  analogWrite(10,random(0,155));
-  analogWrite(9,random(0,155));
-  analogWrite(3,random(0,155));
-  if (millis()%1000==0){
-    digitalWrite (10,HIGH);
-    digitalWrite (9,HIGH);
-    digitalWrite (3,HIGH);
-    delay(500);
+  analogWrite(R_PIN,random(0,255)); //рандомная якрость на красном светодиоде
+  analogWrite(G_PIN,random(0,255)); //рандомная яркость на зеленом светодиоде
+  analogWrite(B_PIN,random(0,255)); //рандомная яркость на синем светодиоде
+  if (millis()%1000==0){            //если с начала работы программы прошло целое количество секунд
+    digitalWrite (R_PIN,HIGH);      //выключаем красный светодиод
+    digitalWrite (G_PIN,HIGH);      //выключаем зеленый светодиод
+    digitalWrite (B_PIN,HIGH);      //выключаем синий светодиод
+    delay(500);                     //ждем полсекунды
   }
 }
